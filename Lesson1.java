0e88a036355dfc0f98a93c41bf9951a2f619353c
natsuki <144816558+natsuki810@users.noreply.github.com>
2024-05-28 22:01:33 +0900

    Java_1

---
+++ Lesson1.java
@@ -0,0 +1,15 @@
+public class Lesson1 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = 3 * 5;
+
+        System.out.println("縦幅３横幅5の長方形の面積は、"+ c);
+
+        blean result = true;
+        char fovriteCharacter ='躯';
+        double pi = 3.14;
+        long number = 314159265853979L;
+        String msg = "ミナトの攻撃！敵に１５ポイントのダメージを与えた！";
+    }
+}
