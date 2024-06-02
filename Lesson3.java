9401fe2bd9f1a54e49c2c901786b56feb9d59276
natsuki <144816558+natsuki810@users.noreply.github.com>
2024-06-03 00:00:39 +0900

    Java_Lesson3練習問題

--- Lesson3.java
+++ Lesson3.java
@@ -1,7 +1,18 @@
 public class Lesson3 {
     public static void main(String[] args) {
-        int age = 18;
-        String s = (age >= 18) ? "成人" : "未成年";
-        System.out.println(s);
+        int isHungry = 1;
+        String food = "オムライス";
+
+        System.out.println("こんにちは");
+
+        if(isHungry == 0){
+            System.out.println("お腹がいっぱいです");
+        } else {
+            System.out.println("腹ペコです");
+        }
+        if(isHungry == 1){
+            System.out.println(food + "をいただきます");
+        }
+        System.out.println("ごちそうさまでした");
     }
 }
