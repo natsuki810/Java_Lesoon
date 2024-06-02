d9ca540a6b95ce81c2ce352d2018857e733fb3e7
natsuki <144816558+natsuki810@users.noreply.github.com>
2024-06-03 00:05:28 +0900

    Java_Lesson3練習問題2

---
+++ Lesson3_2.java
@@ -0,0 +1,12 @@
+public class Lesson3_2 {
+    public static void main(String[] args) {
+        boolean tenki = false;
+        if(tenki == true) {
+            System.out.println("洗濯をします");
+            System.out.println("散歩に行きます");
+        } else {
+            System.out.println("映画を観ます");
+            System.out.println("寝ます");
+        }
+    }
+}
