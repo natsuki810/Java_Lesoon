1bc134baa9780dd860997b6a8865f1805915c130
natsuki <144816558+natsuki810@users.noreply.github.com>
2024-06-12 23:40:11 +0900

    Java_Lesson4_finish

--- Lesson4_7.java
+++ Lesson4_7.java
@@ -1,5 +1,8 @@
 public class Lesson4_7 {
     public static void main(String[] args){
-        
+        int[] points = nwe int[4];
+        double[] weights = double[5];
+        boolean[] answers = boolean[3];
+        Stirng[] names = new Stirng[3];
     }
 }
---
+++ Lesson4_9.java
@@ -0,0 +1,10 @@
+public class Lesson4_9 {
+    public static void main(String[] args) {
+        int[] counts = null;
+        float[] heights = {171.3F, 175.0F};
+        System.out.println(counts[1]);
+        // NullPointerException
+        System.out.println(heights[2]);
+        // ArrayIndexOutOfBoundsException
+    }
+}
