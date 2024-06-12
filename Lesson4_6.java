eb70a89ed2ae137c32d66681fb73b545b5a98411
natsuki <144816558+natsuki810@users.noreply.github.com>
2024-06-12 22:48:14 +0900

    Java_Lesson4

---
+++ Lesson4_6.java
@@ -0,0 +1,8 @@
+public class Lesson4_6 {
+    public static void main(String[] args) {
+        int[][] scores = {{40, 50, 60},{80, 60, 70}};
+        // 二次元配列の初期化
+        System.out.println(scores.length);
+        System.out.println(scores[0].length);
+    }
+}
---
+++ Lesson4_7.java
@@ -0,0 +1,5 @@
+public class Lesson4_7 {
+    public static void main(String[] args){
+        
+    }
+}
